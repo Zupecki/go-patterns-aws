64699d1f0062485235f830cde75e7fb2a0699fc4
Michael Zupecki <michaelzupecki@gmail.com>
2026-03-18 16:13:30 +1100

    Add main runloop

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := run(ctx); err != nil {
+		slog.Error("fatal", "err", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	fmt.Println(ctx.Err())
+	return nil
 }
