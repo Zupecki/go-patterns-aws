f68b7e67251c52e92987b392689b7ad3669c22dd
Michael Zupecki <michaelzupecki@gmail.com>
2026-03-19 13:10:20 +1100

    Add SQS polling helper, switch out test job producer

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"os"
@@ -11,6 +12,10 @@ import (
 	"github.com/Zupecki/go-patterns-aws/internal/jobs"
 	"github.com/Zupecki/go-patterns-aws/internal/store"
 	"github.com/Zupecki/go-patterns-aws/internal/worker"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/google/uuid"
 	"golang.org/x/sync/errgroup"
 )
@@ -30,7 +35,7 @@ func run(ctx context.Context) error {
 	resultsChan := make(chan jobs.Result)
 	errChan := make(chan error, 1) // buffer 1 so cleanup goroutine can send final error without blocking
 	numWorkers := 5
-	numJobs := 10
+	//numJobs := 10
 
 	errorGroup, ctx := errgroup.WithContext(ctx)
 
@@ -46,7 +51,12 @@ func run(ctx context.Context) error {
 	go resultsCleanup(errorGroup, resultsChan, errChan)
 
 	// job producer
-	go produceTestJobs(ctx, jobChan, numJobs)
+	//go produceTestJobs(ctx, jobChan, numJobs)
+	go pollSQS(
+		ctx,
+		"http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/test-queue",
+		jobChan,
+	)
 
 	// results consumer
 	// may want to cancel context if consumer issues, drain workers etc
@@ -105,6 +115,94 @@ func produceTestJobs(ctx context.Context, jobChan chan<- jobs.Job, numJobs int) 
 		case jobChan <- job:
 		case <-ctx.Done():
 			return
+		}
+	}
+}
+
+// SQS
+type QueueMessage struct {
+	Type   string `json:"type"`
+	IntVal int    `json:"intVal,omitempty"`
+	StrVal string `json:"strVal,omitempty"`
+}
+
+func pollSQS(ctx context.Context, queueURL string, jobChan chan<- jobs.Job) error {
+	// create sqs client with AWS SDK
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion("us-east-1"),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("test", "test", "")),
+	)
+	if err != nil {
+		return err
+	}
+
+	sqsClient := sqs.NewFromConfig(cfg, func(o *sqs.Options) {
+		o.BaseEndpoint = aws.String("http://localhost:4566")
+	})
+
+	// long poll
+	sqsParams := sqs.ReceiveMessageInput{
+		QueueUrl:            &queueURL,
+		MaxNumberOfMessages: 10,
+		WaitTimeSeconds:     15,
+	}
+
+	// cancel aware polling
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			out, err := sqsClient.ReceiveMessage(ctx, &sqsParams)
+			if err != nil {
+				return err
+			}
+
+			if len(out.Messages) == 0 {
+				continue
+			}
+
+			for _, m := range out.Messages {
+				if m.Body == nil {
+					continue
+				}
+
+				// parse into Queue struct
+				var qm QueueMessage
+				if err := json.Unmarshal([]byte(*m.Body), &qm); err != nil {
+					return err
+				}
+
+				// parse into Job
+				var job jobs.Job
+				switch qm.Type {
+				case "int":
+					job = jobs.JobProcessInt{
+						ID:     uuid.New(),
+						IntVal: qm.IntVal,
+					}
+				case "string":
+					job = jobs.JobProcessString{
+						ID:     uuid.New(),
+						StrVal: qm.StrVal,
+					}
+				default:
+					return fmt.Errorf("unsupported queue message type for job type")
+				}
+
+				// load Job onto jobChan
+				fmt.Println("Job: ", job)
+
+				// cancel aware job loading
+				select {
+				case jobChan <- job:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+
+				// add delete sqs job message once load successful
+			}
 		}
 	}
 }
