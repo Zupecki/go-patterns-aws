e953f913526165c8933cd243f03b6ce970ba4406
Michael Zupecki <michaelzupecki@gmail.com>
2026-03-20 13:02:13 +1100

    Add dynamo StoreResult method with envelope shape for map conversion re attributevalue for PutItem

--- internal/store/store.go
+++ internal/store/store.go
@@ -2,12 +2,15 @@ package store
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/Zupecki/go-patterns-aws/internal/jobs"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
@@ -31,13 +34,13 @@ func NewLocalStackClient(ctx context.Context) (*dynamodb.Client, error) {
 }
 
 type ResultStore interface {
-	StoreResult(ctx context.Context, result jobs.Result) error
+	StoreResult(ctx context.Context, result jobs.Result, messageID string) error
 }
 
 type PrintStore struct{}
 
-func (s PrintStore) StoreResult(ctx context.Context, r jobs.Result) error {
-	fmt.Printf("Job Result: %v\n", r)
+func (s PrintStore) StoreResult(ctx context.Context, r jobs.Result, messageID string) error {
+	fmt.Printf("Job Result: %v for Message ID: %s\n", r, messageID)
 	return nil
 }
 
@@ -49,8 +52,41 @@ func NewDynamoStore(db *dynamodb.Client) *DynamoStore {
 	return &DynamoStore{db: db}
 }
 
-func (s DynamoStore) StoreResult(ctx context.Context, r jobs.Result) error {
-	fmt.Printf("Job Result: %v\n", r)
+type ResultItem struct {
+	JobID      string `dynamodbav:"job_id"`
+	MessageID  string `dynamodbav:"message_id"`
+	JobType    string `dynamodbav:"job_type"`
+	ResultJSON string `dynamodbav:"result_json"`
+	CreatedAt  string `dynamodbav:"created_at"`
+}
+
+func (s *DynamoStore) StoreResult(ctx context.Context, r jobs.Result, messageID string) error {
+	resultJSON, err := json.Marshal(r)
+	if err != nil {
+		return fmt.Errorf("marshal result json: %w", err)
+	}
+
+	item := ResultItem{
+		JobID:      r.JobID().String(),
+		MessageID:  messageID,
+		JobType:    string(r.JobType()),
+		ResultJSON: string(resultJSON),
+		CreatedAt:  time.Now().UTC().Format(time.RFC3339),
+	}
+
+	av, err := attributevalue.MarshalMap(item)
+	if err != nil {
+		return err
+	}
+
+	putItemInput := dynamodb.PutItemInput{
+		Item: av,
+	}
+
+	_, err = s.db.PutItem(ctx, &putItemInput)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
